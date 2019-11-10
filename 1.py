6d411ce697416c3862f0750161b6e7b511d5cc15
Kadir Erdoğan <57061024+b21727207@users.noreply.github.com>
2019-11-10 21:21:03 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,7 +4,6 @@ This program helps to find the roots of a second-order equation
 a=x-squared term
 b=x coefficient term
 c=invariable 
-
 *******************
 """)
 a=int(input("Please enter a:"))
@@ -14,13 +13,9 @@ discriminant=b**2-4*a*c
 
 if discriminant<0:
     print("Real root does not exist!")
-
-
 elif discriminant==0:
     print("quadratic has a repeated real number solution")
-    print("x1==", (-b - discriminant ** 1 / 2) / 2 * a, ",", "x2==", (-b + discriminant ** 1 / 2) / 2 * a)
-
+    print("x1=",(-b-discriminant**(1/2))/2*a,"x2=",(-b+discriminant**(1/2))/2*a)
 else:
     print("the quadratic has two distinct real number solutions")
-    print("x1==", (-b - discriminant ** 1 / 2) / 2 * a, ",", "x2==", (-b + discriminant ** 1 / 2) / 2 * a)
-
+    print("x1=",(-b-discriminant**(1/2))/2*a,"x2=",(-b+discriminant**(1/2))/2*a)
